bd3c7a7cb5b0859d3653ba8396c6a195caca0bb2
Remo Pellegrino <pellere@cronos.be>
2023-12-13 14:37:27 +0100

    First setup cucumber/typescript (WIP)

---
+++ features/ExampleFeature.feature
@@ -0,0 +1,6 @@
+Feature: Example Feature
+
+Scenario: Run a simple scenario
+  Given I have a simple scenario
+  When I run the scenario
+  Then I should see the result
---
+++ features/step_definitions/ExampleFeature.ts
@@ -0,0 +1,16 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('I have a simple scenario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+When('I run the scenario', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+Then('I should see the result', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
